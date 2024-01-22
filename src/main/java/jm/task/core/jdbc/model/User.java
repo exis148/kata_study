ce9221f06a7bb77e491dc1b414d4999b3ffaf3fb
Vladislav Zhurnalev <vkadzhu@gmail.com>
2024-01-22 16:21:28 +0300

    adding annotations to the User class

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -4,18 +4,20 @@ import javax.persistence.*;
 import java.util.Objects;
 
 @Table
+@Entity
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
+
+    @Column(name = "name")
     private String name;
 
-    @Column
+    @Column(name = "last_name")
     private String lastName;
 
-    @Column
+    @Column(name = "age")
     private Byte age;
 
     public User() {
